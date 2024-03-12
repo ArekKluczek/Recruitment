aa8f73572415a57014e06ac0b9cf4794733a5fa7
Your Name <s24307@pjwstk.edu.pl>
2024-03-12 21:40:46 +0100

    Project setup fixes

--- .ddev/commands/host/init.sh
+++ .ddev/commands/host/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ddev composer install
-ddev yarn install
+ddev yarn install --ignore-engines
 ddev yarn encore dev
 ddev php bin/console doctrine:migrations:migrate
 ddev import-db --file=db.sql
